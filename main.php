ecb6e2b6c0a8a6bd7b18d84e9490d4d84827c5fb
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-01-26 13:08:15 +0530

    page for adding new review

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add Review Page</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div style="text-align:center" >
+  <h1>Add review</h1>
+  <br><br>
+  <form action="submit_form.php" method="post">
+    Name of Product : <input type="text" name="productname" autofocus><br><br>
+    Rating (between 0 to 10) : <input type="number" name="rating" step="0.01" min="0" max="10.1" autocomplete="off"><br><br>
+    Comment : <textarea name="comment" rows="5" cols="50"></textarea><br><br>
+    <input type="submit" value="Submit Review">
+  </form>
+<br>
+  <form method="get" action="/">
+    <button type="submit">Existing Reviews</button>
+  </form>
+</div>
+
+</body>
+</html>
---
+++ submit_form.php
@@ -0,0 +1,45 @@
+<?php
+  // $name = $_POST["productname"];
+  // $rating = $_POST["rating"];
+  // $comment = $_POST["comment"];
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "abc";
+  $dbname = "db1";
+
+  $conn = new mysqli($servername,$username,$password,$dbname);
+  if($conn->connect_error){
+    die("Connection to database failed : ".$conn->error);
+  }
+  else {
+    echo "connected to database ".$dbname;
+  }
+
+  $name=$_POST["productname"];
+  $rating=$_POST["rating"];
+  $comment=$_POST["comment"];
+  $comment=testinputdata($comment);
+  $sql="insert into comments (product_name, user_rating, comment) values (\"$name\",$rating,\"$comment\")";
+  //echo "</br>".$sql."</br>";
+
+  if($conn->query($sql)===true){
+    echo "<br>successful query<br>".$sql;
+  }
+  else {
+    echo "<br>error in entering data to db :<br>".$conn->error;
+  }
+
+$newURL="main.php";
+header('Location: '.$newURL);
+die();
+
+  function testinputdata($data){
+    $data=trim($data);
+    $data=stripslashes($data);
+    $data=htmlspecialchars($data);
+    return $data;
+  }
+
+  $conn->close();
+?>
